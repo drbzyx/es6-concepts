281cbc267e25d337df957e004545889070ff2a24
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-22 16:11:45 +0100

    Index: Add navigation list

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,4 +1,4 @@
-import { Heading, Main, Paragraph } from '../components'
+import { Heading, List, Main, Paragraph } from '../components'
 
 export default function Home() {
   return (
@@ -10,6 +10,34 @@ export default function Home() {
         using these concepts for years but never taken the time to understand
         why.
       </Paragraph>
+      <List
+        items={[
+          { url: '/ecma-2015', text: 'What is ES6 or ECMAScript 2015?' },
+          { url: '/features', text: 'What did ES6 introduce?' },
+          { url: '/let-and-const', text: 'What is let and const?' },
+          { url: '/arrow-functions', text: 'What are arrow functions?' },
+          { url: '/spread-operator', text: 'What is the spead operator?' },
+          { url: '/template-literals', text: 'What are template literals?' },
+          { url: '/destructuring', text: 'What is destructuring?' },
+          {
+            url: '/iife',
+            text: 'What are immediately invoked function expressions?',
+          },
+          { url: '/classes', text: 'What are classes?' },
+          { url: '/generators', text: 'What are generator functions?' },
+          { url: '/loops', text: 'What are the ES6 loops?' },
+          { url: '/maps', text: 'What are maps?' },
+          { url: '/weak-things', text: 'What are weaksets and weakmaps?' },
+          { url: '/promises', text: 'What are promises?' },
+          { url: '/callbacks', text: 'What are callbacks?' },
+          { url: '/modules', text: 'What are modules?' },
+          { url: '/hoisting', text: 'What is hoisting?' },
+          { url: '/array-methods', text: 'What are the new array methods?' },
+          { url: '/string-methods', text: 'What are the new string methods?' },
+          { url: '/babel', text: 'What is Babel?' },
+          { url: '/webpack', text: 'What is Webpack?' },
+        ]}
+      />
     </Main>
   )
 }
