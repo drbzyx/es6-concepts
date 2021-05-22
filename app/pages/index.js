99675e2262b2f144286cc38f772ce65cb5276f3f
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-22 15:42:42 +0100

    Index: Update introduction content

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,11 +1,15 @@
-import { Heading, Main } from '../components'
+import { Heading, Main, Paragraph } from '../components'
 
 export default function Home() {
   return (
-    <div className='app'>
-      <Main>
-        <Heading>Home</Heading>
-      </Main>
-    </div>
+    <Main>
+      <Heading>Home</Heading>
+      <Paragraph>
+        I'm covering some of the common ES6 interview questions. Mostly as
+        preparation for myself and to learn the ins and outs better. I've been
+        using these concepts for years but never taken the time to understand
+        why.
+      </Paragraph>
+    </Main>
   )
 }
