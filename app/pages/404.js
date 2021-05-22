35a6ec19b49fae52e0f1dd7fc19f513a6def755c
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-22 16:15:25 +0100

    Pages: Refactor 404 with Components

--- app/pages/404.js
+++ app/pages/404.js
@@ -1,13 +1,15 @@
+import { Heading, Main, Paragraph } from '../components'
+
 import Link from 'next/link'
 
 export default function Custom404() {
   return (
-    <main>
-      <h1>404</h1>
-      <p>You won't be able to do much here.</p>
-      <p>
+    <Main>
+      <Heading>404</Heading>
+      <Paragraph>You won't be able to do much here.</Paragraph>
+      <Paragraph>
         It's probably best to go <Link href='/'>home</Link>.
-      </p>
-    </main>
+      </Paragraph>
+    </Main>
   )
 }
