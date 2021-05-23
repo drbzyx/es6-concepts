f437f230087cf8e05707ebee8be174b071897e99
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-23 10:47:24 +0100

    Pages(Keywords): Add const and let keyword content

--- app/pages/let-and-const.js
+++ app/pages/let-and-const.js
@@ -1,11 +1,30 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import {
+  Code,
+  Heading,
+  Main,
+  Paragraph,
+  Section,
+  Subheading,
+} from '../components'
 
 export default function LetAndConst() {
   return (
     <Main>
       <Heading>What is let and const?</Heading>
       <Section>
-        <Paragraph>What is let and const?</Paragraph>
+        <Subheading>Let</Subheading>
+        <Paragraph>
+          As it was introduced, <Code>let</Code> was the new <Code>var</Code>.
+          It's the keyword which allows you to declare a variable with block
+          scope.
+        </Paragraph>
+      </Section>
+      <Section>
+        <Subheading>Const</Subheading>
+        <Paragraph>
+          The <Code>const</Code> keyword is similar to let, it's block scoped
+          too. Except it's used for values which can't be changed.
+        </Paragraph>
       </Section>
     </Main>
   )
