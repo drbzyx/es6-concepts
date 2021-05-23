579060d5939ea6ffe1811b8d5a3a6ed77c481a0b
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-23 10:34:04 +0100

    Pages(ES6): Update content

--- app/pages/ecma-2015.js
+++ app/pages/ecma-2015.js
@@ -1,19 +1,43 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import {
+  Heading,
+  List,
+  Main,
+  Paragraph,
+  Section,
+  Subheading,
+} from '../components'
 
 export default function Ecma() {
+  const items = [
+    { text: 'Arrow functions' },
+    { text: 'Destructuring' },
+    { text: 'Generators and iterators' },
+    { text: 'Let and const keywords' },
+    { text: 'Modules and classes' },
+    { text: 'Object literals' },
+    { text: 'Promises' },
+    { text: 'Rest and spread operators' },
+    { text: 'Template literals' },
+  ]
+
   return (
     <Main>
       <Heading>What is ES6 or ECMAScript 2015?</Heading>
       <Section>
+        <Subheading>Well, what is it?</Subheading>
         <Paragraph>
           ECMAScript is a JavaScript standard meant to ensure interoperability
-          between browsers.
+          between browsers.ES6 was the second major revision of JavaScript
+          released in 2015.
         </Paragraph>
         <Paragraph>
-          ES6 was the second major revision of JavaScript released in 2015. It
-          included a lot of new features such as modules, new iterators, new
+          It included a lot of new features such as modules, new iterators, new
           classes, arrow functions, spreading, template literals and more.
         </Paragraph>
+      </Section>
+      <Section>
+        <Subheading>New Features</Subheading>
+        <List items={items} />
       </Section>
     </Main>
   )
