e168d5728e9726570b459c568dc9d0c718017aab
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-23 10:23:45 +0100

    Pages(ES6): Add content

--- app/pages/ecma-2015.js
+++ app/pages/ecma-2015.js
@@ -5,7 +5,15 @@ export default function Ecma() {
     <Main>
       <Heading>What is ES6 or ECMAScript 2015?</Heading>
       <Section>
-        <Paragraph>What is ES6 or ECMAScript 2015?</Paragraph>
+        <Paragraph>
+          ECMAScript is a JavaScript standard meant to ensure interoperability
+          between browsers.
+        </Paragraph>
+        <Paragraph>
+          ES6 was the second major revision of JavaScript released in 2015. It
+          included a lot of new features such as modules, new iterators, new
+          classes, arrow functions, spreading, template literals and more.
+        </Paragraph>
       </Section>
     </Main>
   )
