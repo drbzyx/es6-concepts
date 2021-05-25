92c15e38b91a90eab5b6a4fe177e9d445b9ce1de
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-25 14:09:36 +0100

    Pages(Template Literals): Add content

--- app/pages/template-literals.js
+++ app/pages/template-literals.js
@@ -1,11 +1,25 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import { Code, Heading, Main, Paragraph, Section } from '../components'
 
 export default function TemplateLiterals() {
   return (
     <Main>
       <Heading>What are Template Literals?</Heading>
       <Section>
-        <Paragraph>What are Template Literals?</Paragraph>
+        <Paragraph>
+          Template literals are a new method of multi-line strings and string
+          interopolation. This makes strings a lot easier and prettier.
+        </Paragraph>
+        <Paragraph>
+          For example lets take the <Code>name</Code> 'Dan' and throw him in
+          some strings. The "old" way could have been something like this:{' '}
+          <Code>
+            const string = 'My name is ' + name + ', nice to see you.'
+          </Code>{' '}
+          but now we can do{' '}
+          <Code>
+            {'const template = `My name is ${name}, nice to see you.`'}
+          </Code>
+        </Paragraph>
       </Section>
     </Main>
   )
