81459231d2079e574a5aa52ae351d0a47d18064d
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-25 13:54:52 +0100

    Pages(Callbacks): Add content

--- app/pages/callbacks.js
+++ app/pages/callbacks.js
@@ -1,12 +1,27 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import { useState } from 'react'
+import { Button, Code, Heading, Main, Paragraph, Section } from '../components'
 
 export default function Callbacks() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => setCount(count + 1)
+
   return (
     <Main>
       <Heading>What are Callbacks?</Heading>
       <Section>
-        {/* Callbacks, Callback hell */}
-        <Paragraph>Callbacks</Paragraph>
+        <Paragraph>
+          Callbacks are functions passed as an argument to another function...
+          which is then called. A pretty good example of callback usage would be
+          those called by events in React{' '}
+          <Code>{`<Button changeHandler={handleClick}>Clicked {count} times</Button>`}</Code>
+        </Paragraph>
+        <Button handler={handleClick}>Clicked {count} times</Button>
+        <Paragraph>
+          As some applications get larger and larger, following the sequence of
+          callbacks may get harder and harder. This is commonly referred to as
+          callback hell.
+        </Paragraph>
       </Section>
     </Main>
   )
