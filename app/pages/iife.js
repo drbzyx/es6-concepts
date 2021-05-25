532e89cb02454d7ce98660ca200e620771729f97
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-25 14:43:14 +0100

    Pages(IIFE): Add content

--- app/pages/iife.js
+++ app/pages/iife.js
@@ -1,4 +1,4 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import { Code, Heading, Main, Paragraph, Section } from '../components'
 
 export default function IIFE() {
   return (
@@ -6,7 +6,15 @@ export default function IIFE() {
       <Heading>What are immediately invoked function expressions?</Heading>
       <Section>
         <Paragraph>
-          What are immediately invoked function expressions?
+          These functions are run as soon as they are "defined" and are
+          self-executing. They look quite messy{' '}
+          <Code>{`(function () { console.log('I am called immediately') })()`}</Code>{' '}
+          or <Code>{`(() => { /* ... */ })()`}</Code>.
+        </Paragraph>
+        <Paragraph>
+          These functions can have parameters. For example take an array, and
+          pass it in.{' '}
+          <Code>{`((...args) => console.log(args))(...array)`}</Code>
         </Paragraph>
       </Section>
     </Main>
