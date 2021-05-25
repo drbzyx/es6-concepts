b3a55853e547fa69ec1a0cb8c521808519db37d1
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-25 13:41:49 +0100

    Pages(Modules): Add content

--- app/pages/modules.js
+++ app/pages/modules.js
@@ -1,11 +1,22 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import { Code, Heading, Main, Paragraph, Section } from '../components'
 
 export default function Modules() {
   return (
     <Main>
       <Heading>What are Modules?</Heading>
       <Section>
-        <Paragraph>Modules in ES6</Paragraph>
+        <Paragraph>
+          Modules are functions, variables, anything which are in essence
+          self-contained code with specific functionality; which is exported and
+          then imported throughout an app.
+        </Paragraph>
+        <Paragraph>
+          I like defining whatever I need a file, and export them together in an
+          object after the fact. React components are generally default
+          exported. For example{' '}
+          <Code>{`export default function Component() { ... }`}</Code> and{' '}
+          <Code>{`export { function, variable }`}</Code>.
+        </Paragraph>
       </Section>
     </Main>
   )
