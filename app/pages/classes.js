4f50be800524622b7579e793a8780fcdda2a64d3
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-25 14:26:46 +0100

    Pages(Classes): Add content

--- app/pages/classes.js
+++ app/pages/classes.js
@@ -1,11 +1,25 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import { Code, Heading, Main, Paragraph, Section } from '../components'
 
 export default function Classes() {
   return (
     <Main>
       <Heading>What are Classes?</Heading>
       <Section>
-        <Paragraph>What are Classes?</Paragraph>
+        <Paragraph>
+          Classes are a template for creating objects. These are made with a{' '}
+          <Code>constructor</Code> and are encapsulated. Class declarations are
+          not hoisted, you might get a <Code>ReferenceError</Code>! I haven't
+          really felt the need to use these often since React moved away from
+          them.
+        </Paragraph>
+        <Paragraph>
+          Functions of a class are used to perform actions of that specific
+          class instance.
+        </Paragraph>
+        <Paragraph>
+          Constructors in classes allocate the memory to the objects of that
+          specific class.
+        </Paragraph>
       </Section>
     </Main>
   )
