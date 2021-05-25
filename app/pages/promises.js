d2822a42298e967e7239e1ef605a5b12c825962c
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-25 11:09:59 +0100

    Pages(Promises): Add content

--- app/pages/promises.js
+++ app/pages/promises.js
@@ -1,11 +1,28 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import { Code, Heading, Main, Paragraph, Section } from '../components'
 
 export default function Promises() {
   return (
     <Main>
       <Heading>What are Promises?</Heading>
       <Section>
-        <Paragraph>Promises in ES6</Paragraph>
+        <Paragraph>
+          A Promise is an object which representations the eventual completion
+          or failure of an asynchronous operation and its returned value. You
+          can create promises by doing:{' '}
+          <Code>{`const promise = new Promise((resolve, reject) => { resolve('something') })`}</Code>
+        </Paragraph>
+        <Paragraph>
+          Promises can have one of three values; <Code>pending</Code>,{' '}
+          <Code>fulfilled</Code> or <Code>rejected</Code>.{' '}
+          <Code>promise.then()</Code>,<Code>promise.catch()</Code>, and{' '}
+          <Code>promise.finally()</Code> are used to chain further action with a
+          promise.
+        </Paragraph>
+        <Paragraph>
+          You can <Code>await</Code>a promise like so:{' '}
+          <Code>const value = await promise</Code> when wrapped in a{' '}
+          <Code>async</Code> function.
+        </Paragraph>
       </Section>
     </Main>
   )
