e4a80a2704b9eeee15d61167ba0eff7c179e76e1
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-29 16:08:32 +0100

    Pages(Maps): Add Sets, WeakSets, and WeakMap content

--- app/pages/maps.js
+++ app/pages/maps.js
@@ -1,10 +1,19 @@
-import { Code, Heading, Main, Paragraph, Section } from '../components'
+import {
+  Code,
+  Heading,
+  Main,
+  Paragraph,
+  Section,
+  Subheading,
+} from '../components'
+import Link from 'next/link'
 
 export default function Maps() {
   return (
     <Main>
-      <Heading>Maps in ES6</Heading>
+      <Heading>Collections</Heading>
       <Section>
+        <Subheading>Maps</Subheading>
         <Paragraph>
           A Map in JavaScript is an object with key and value pairs. Maps
           remember the original insertion order of the keys.
@@ -32,7 +41,61 @@ export default function Maps() {
         </Paragraph>
         <Paragraph>
           Maps can be iterated using{' '}
-          <Code>{'for (const value of sandwiches) { /* ... */ }'}</Code>
+          <Code>{'for (const value of sandwiches) { ... }'}</Code>
+        </Paragraph>
+      </Section>
+      <Section>
+        <Subheading>WeakMaps</Subheading>
+        <Paragraph>
+          A <Code>WeakMap</Code> is basically just a <Code>Map</Code> except it
+          has a few differences. The <Code>key</Code> must be an object. The{' '}
+          <Code>key</Code> is weakly referenced. A <Code>WeakMap</Code> is a
+          black box; there's no way retrieve the list of keys or see its size.
+          You cannot loop over these with <Code>for...of</Code> either. The{' '}
+          <Code>value</Code> is accessible using the <Code>key</Code> reference.
+        </Paragraph>
+        <Paragraph>
+          There are only 4 methods: <Code>get(key)</Code>,{' '}
+          <Code>set(key, value)</Code>, <Code>delete(key)</Code> and{' '}
+          <Code>has(key)</Code>.
+        </Paragraph>
+        <Paragraph>
+          In examples I have seen, if reference to the original object is
+          destroyed, the value of the <Code>WeakSey</Code> with that key will
+          also be removed and garbage collected: For example;{' '}
+          <Link href='https://javascript.plainenglish.io/weakmaps-illustrated-8169ce4764bb'>
+            this example
+          </Link>{' '}
+          and{' '}
+          <Link href='https://javascript.info/weakmap-weakset#weakmap'>
+            this one (which is better)
+          </Link>
+          .
+        </Paragraph>
+      </Section>
+      <Section>
+        <Subheading>Sets</Subheading>
+        <Paragraph>
+          A <Code>Set</Code> is a collection of unique values. They can be
+          iterated through in the order they were inserted. Sets can be iterated
+          using <Code>{'for (const value of set) { ... }'}</Code>.
+        </Paragraph>
+        <Paragraph>
+          <Code>Map</Code> and <Code>Set</Code> share the same methods:{' '}
+          <Code>set.add(value)</Code>,<Code>set.has(value)</Code>,{' '}
+          <Code>set.delete(value)</Code>, <Code>set.size</Code> and they can be
+          iterated.
+        </Paragraph>
+      </Section>
+      <Section>
+        <Subheading>WeakSets</Subheading>
+        <Paragraph>
+          Similar to the <Code>WeakMap</Code> discussion above, here we follow a
+          similar pattern. We may only add objects, an object may exist in the{' '}
+          <Code>WeakSet</Code> if it's reachable from somewhere else, and it
+          only supports <Code>add</Code>, <Code>has</Code>, <Code>delete</Code>;
+          it doesn't have any iterators or have <Code>values()</Code> or{' '}
+          <Code>keys()</Code>.
         </Paragraph>
       </Section>
     </Main>
