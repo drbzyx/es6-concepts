b9027328cbb4903e2be0e383ad241ff7a48752bf
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-29 18:08:26 +0100

    Pages(Strings): Add content

--- app/pages/string-methods.js
+++ app/pages/string-methods.js
@@ -1,11 +1,15 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import { Code, Heading, Main, Paragraph, Section } from '../components'
 
 export default function StringMethods() {
   return (
     <Main>
       <Heading>What are the new string methods?</Heading>
       <Section>
-        <Paragraph>String methods in ES6</Paragraph>
+        <Paragraph>
+          Strings have plenty of methods, but ES6 introduced four new ones;{' '}
+          <Code>startsWith(string)</Code>, <Code>endsWith(string)</Code>,{' '}
+          <Code>includes(string)</Code> and <Code>repeat(int)</Code>.
+        </Paragraph>
       </Section>
     </Main>
   )
