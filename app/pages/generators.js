163d0bee12774279bb333a664d9251f2d108e4a2
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-29 17:35:12 +0100

    Pages(Generator): Add generator function content

--- app/pages/generators.js
+++ app/pages/generators.js
@@ -1,11 +1,32 @@
-import { Heading, Main, Paragraph, Section } from '../components'
+import { Code, Heading, Main, Paragraph, Section } from '../components'
 
 export default function Generators() {
   return (
     <Main>
       <Heading>What are Generators?</Heading>
       <Section>
-        <Paragraph>What are Generators?</Paragraph>
+        <Paragraph>
+          Generators are a new function introduced by ES6 which can be suspended
+          and return different values depending which time it has been called.
+          Generator functions are denoted using an asterisk;{' '}
+          <Code>{'function* generateSomething() { ... }'}</Code>.
+        </Paragraph>
+        <Paragraph>
+          The keyword <Code>yield 'randomValue'</Code> is used to suspend the
+          function and return the value to the caller. Calling the function
+          again would resume from after that statement.
+        </Paragraph>
+        <Paragraph>
+          The <Code>next()</Code> method is the main method of these generators.
+          Calling this method resumes the generator function up until the next{' '}
+          <Code>yield</Code>. This function returns the <Code>value</Code> from
+          the next <Code>yield</Code>, and a <Code>done</Code> boolean to say
+          whether there is more statements to come.
+        </Paragraph>
+        <Paragraph>
+          You can even use the <Code>for...of</Code> loop to iterate over values
+          returned from generator functions which is quite cool.
+        </Paragraph>
       </Section>
     </Main>
   )
