ac8b7a1c8caf9ce36659d97dcad88f867b842f0e
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2022-01-08 10:19:03 +0000

    Update arrow-functions.js

--- app/pages/arrow-functions.js
+++ app/pages/arrow-functions.js
@@ -1,4 +1,4 @@
-import { Code, Heading, Main, Paragraph, Section } from '../components'
+import { CodeSnippit, Heading, Main, Paragraph, Section } from '../components'
 
 export default function ArrowFunctions() {
   return (
@@ -9,28 +9,31 @@ export default function ArrowFunctions() {
           Arrow functions are a shorthand notation for writing ES6 functions.
           They are rather flexible in terms of how they are written and reduce
           the code size. Small arrow functions don't need to use the{' '}
-          <Code>return</Code> keyword.
+          <CodeSnippit code="return" /> keyword.
         </Paragraph>
         <Paragraph>
           Example of a simple arrow function which console logs some arguements:{' '}
-          <Code>{`const printArgs = (...args) => { console.log(args) }`}</Code>
+          <CodeSnippit
+            code={`const printArgs = (...args) => { console.log(args) }`}
+          />
+          .
         </Paragraph>
         <Paragraph>
           Due to the fact this is a one liner, you could ditch the{' '}
-          <Code>{`{ ... }`}</Code> like:{' '}
-          <Code>{`const returnArgs = (...args) => args`}</Code>
+          <CodeSnippit code={`{ ... }`} /> like:{' '}
+          <CodeSnippit code={`const returnArgs = (...args) => args`} />.
         </Paragraph>
         <Paragraph>
           They can be written anonymously too, just like regular functions.
           Instead of doing{' '}
-          <Code>{`function() { console.log('something')}`}</Code> we could do{' '}
-          <Code>{`() => console.log('wow')`}</Code>
+          <CodeSnippit code={`function() { console.log('something')}`} /> we
+          could do <CodeSnippit code={`() => console.log('wow')`} />
         </Paragraph>
         <Paragraph>
           Arrow functions are context bound. We wouldn't need to implicitly bind{' '}
-          <Code>this</Code> because it is performed automatically by the arrow
-          function, <Code>this</Code> representing the object that defined the
-          function.
+          <CodeSnippit code="this" /> because it is performed automatically by
+          the arrow function, <CodeSnippit code="this" /> representing the
+          object that defined the function.
         </Paragraph>
       </Section>
     </Main>
