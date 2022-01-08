d8fa09993ab7cb1d64299e201895f3a8afe17bb2
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2022-01-08 09:49:45 +0000

    Update generators.js

--- app/pages/generators.js
+++ app/pages/generators.js
@@ -1,4 +1,4 @@
-import { Code, Heading, Main, Paragraph, Section } from '../components'
+import { CodeSnippit, Heading, Main, Paragraph, Section } from '../components'
 
 export default function Generators() {
   return (
@@ -9,23 +9,24 @@ export default function Generators() {
           Generators are a new function introduced by ES6 which can be suspended
           and return different values depending which time it has been called.
           Generator functions are denoted using an asterisk;{' '}
-          <Code>{'function* generateSomething() { ... }'}</Code>.
+          <CodeSnippit code={'function* generateSomething() { ... }'} />.
         </Paragraph>
         <Paragraph>
-          The keyword <Code>yield</Code> is used to suspend the function and
-          return a value. Calling the function again would resume from after
-          that statement.
+          The keyword <CodeSnippit code="yield" /> is used to suspend the
+          function and return a value. Calling the function again would resume
+          from after that statement.
         </Paragraph>
         <Paragraph>
-          The <Code>next()</Code> method is the main method of these generators.
-          Calling this method resumes the generator function up until the next{' '}
-          <Code>yield</Code>. This function returns the <Code>value</Code> from
-          the next <Code>yield</Code>, and a <Code>done</Code> boolean to say
-          whether there is more statements to come.
+          The <CodeSnippit code="next()" /> method is the main method of these
+          generators. Calling this method resumes the generator function up
+          until the next <CodeSnippit code="yield" />. This function returns the{' '}
+          <CodeSnippit code="value" /> from the next{' '}
+          <CodeSnippit code="yield" />, and a <CodeSnippit code="done" />{' '}
+          boolean to say whether there is more statements to come.
         </Paragraph>
         <Paragraph>
-          You can even use the <Code>for...of</Code> loop to iterate over values
-          returned from generator functions which is quite cool.
+          You can even use the <CodeSnippit code="for...of" /> loop to iterate
+          over values returned from generator functions which is quite cool.
         </Paragraph>
       </Section>
     </Main>
