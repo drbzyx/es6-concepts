6b43bc2d4f7b83d7cf031f3f2fb0cb9de3567122
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2022-01-08 09:44:58 +0000

    Update iife.js

--- app/pages/iife.js
+++ app/pages/iife.js
@@ -1,4 +1,4 @@
-import { Code, Heading, Main, Paragraph, Section } from '../components'
+import { CodeSnippit, Heading, Main, Paragraph, Section } from '../components'
 
 export default function IIFE() {
   return (
@@ -8,13 +8,15 @@ export default function IIFE() {
         <Paragraph>
           These functions are run as soon as they are "defined" and are
           self-executing. They look quite messy{' '}
-          <Code>{`(function () { console.log('I am called immediately') })()`}</Code>{' '}
-          or <Code>{`(() => { /* ... */ })()`}</Code>.
+          <CodeSnippit
+            code={`(function () { console.log('I am called immediately') })()`}
+          />{' '}
+          or <CodeSnippit code={`(() => { /* ... */ })()`} />.
         </Paragraph>
         <Paragraph>
           These functions can have parameters. For example take an array, and
           pass it in.{' '}
-          <Code>{`((...args) => console.log(args))(...array)`}</Code>
+          <CodeSnippit code={`((...args) => console.log(args))(...array)`} />.
         </Paragraph>
       </Section>
     </Main>
