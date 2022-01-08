8a2853e903df0079f93c8ac8056464fa6ff30aa2
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2022-01-08 09:53:14 +0000

    Update destructuring.js

--- app/pages/destructuring.js
+++ app/pages/destructuring.js
@@ -1,4 +1,4 @@
-import { Code, Heading, Main, Paragraph, Section } from '../components'
+import { CodeSnippit, Heading, Main, Paragraph, Section } from '../components'
 
 export default function Destructuring() {
   return (
@@ -9,25 +9,27 @@ export default function Destructuring() {
           Destructuring is an expression which makes it possible to unpack
           values from arrays or objects, or declare them. Usually this solves
           the problem of repeating the object dot annotation{' '}
-          <Code>{`object.property`}</Code>.
+          <CodeSnippit code={`object.property`} />.
         </Paragraph>
         <Paragraph>
           You can use destructuring assignment by declaring variables{' '}
-          <Code>let ten, twenty</Code> and then assigning values{' '}
-          <Code>[ten, twenty] = [10, 20]</Code>.
+          <CodeSnippit code="let ten, twenty" /> and then assigning values{' '}
+          <CodeSnippit code="[ten, twenty] = [10, 20]" />.
         </Paragraph>
         <Paragraph>
           To destructure an object such as{' '}
-          <Code>{`const object = { a: 'a', b: 'b', c: 'c' }`}</Code> you can
-          simply <Code>{`const { a, b, c } = object`}</Code> which means you
-          then don't need to use <Code>object.a</Code> to refer to the value.
+          <CodeSnippit code={`const object = { a: 'a', b: 'b', c: 'c' }`} /> you
+          can simply <CodeSnippit code={`const { a, b, c } = object`} /> which
+          means you then don't need to use <CodeSnippit code="object.a" /> to
+          refer to the value.
         </Paragraph>
         <Paragraph>
           To destructure an array such as{' '}
-          <Code>const array = ['random', 'variable', 'names']</Code> you can do{' '}
-          <Code>const [anything, you, want] = array</Code>. Note that to
-          destructure objects, you use <Code>{`{}`}</Code> and arrays{' '}
-          <Code>{`[]`}</Code>; they would be undefined values otherwise.
+          <CodeSnippit code="const array = ['random', 'variable', 'names']" />{' '}
+          you can do <CodeSnippit code="const [anything, you, want] = array" />.
+          Note that to destructure objects, you use <CodeSnippit code={`{}`} />{' '}
+          and arrays <CodeSnippit code={`[]`} />; they would be undefined values
+          otherwise.
         </Paragraph>
       </Section>
     </Main>
